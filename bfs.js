ceab8f392f7e5344f644dec07b822118de1abd35
Ashawe <harshsaglani@gmail.com>
2020-04-08 18:11:59 +0530

    :sparkles: Implement Wall creation

--- bfs.js
+++ bfs.js
@@ -14,4 +14,44 @@ $( document ).ready(function() {
 			$("#r" + i + "").append(cell_part1 + "" + i + "c" + j + "" + cell_part2);
     	}
     }
+
+    $(document).mousedown(function(){
+    	window.IS_CLICKED = true;
+    });
+
+    $(document).mouseup(function(){
+    	window.IS_CLICKED = false;
+    });
+
+    function dragger(event,state){
+		if(window.IS_CLICKED  || state == "down" ){
+			if(state == "down"){	
+				if(window.IS_SELECTING_START){
+					$(event.target).addClass("start");
+					window.IS_SELECTING_START = false;
+					window.IS_START_SELECTED = true;
+				}
+				if(window.IS_SELECTING_END){
+					$(event.target).addClass("end");
+					window.IS_SELECTING_END = false;
+					window.IS_END_SELECTED = true;
+				}
+				if(window.IS_START_SELECTED && window.IS_END_SELECTED)
+					window.IS_SELECTED = true;
+			}
+			if( $(event.target).attr("class") == "cell" || $(event.target).attr("class") == "cell wall" ){
+				$(event.target).toggleClass("wall");
+			}
+		}
+    }
+
+    $(".cell").each(function(){
+    	$(this).mousedown(function(e){
+    		dragger(e,"down");
+    	});
+    	$(this).mouseenter(function(e){
+    		dragger(e,"enter");
+    	});
+    });
+
 });
